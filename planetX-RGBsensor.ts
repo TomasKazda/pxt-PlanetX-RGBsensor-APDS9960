dbf66f7677853cfc760f07df3a97a3dc779755d3
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-12-23 16:51:14 +0100

    planetX sensor fragment data

---
+++ planetX-RGBsensor.ts
@@ -0,0 +1,182 @@
+/**
+* Functions to PlanetX sensor by ELECFREAKS Co.,Ltd.
+*/
+//% color=#00B1ED  icon="\uf005" block="PlanetX_RGBsensor" blockId="PlanetX_RGBsensor"
+namespace PlanetX_RGBsensor {
+    export enum ColorList {
+        //% block="Red"
+        red,
+        //% block="Green"
+        green,
+        //% block="Blue"
+        blue,
+        //% block="Cyan"
+        cyan,
+        //% block="Magenta"
+        magenta,
+        //% block="Yellow"
+        yellow,
+        //% block="White"
+        white
+    }
+
+    /////////////////////////color/////////////////////////
+    const APDS9960_ADDR = 0x39
+    const APDS9960_ENABLE = 0x80
+    const APDS9960_ATIME = 0x81
+    const APDS9960_CONTROL = 0x8F
+    const APDS9960_STATUS = 0x93
+    const APDS9960_CDATAL = 0x94
+    const APDS9960_CDATAH = 0x95
+    const APDS9960_RDATAL = 0x96
+    const APDS9960_RDATAH = 0x97
+    const APDS9960_GDATAL = 0x98
+    const APDS9960_GDATAH = 0x99
+    const APDS9960_BDATAL = 0x9A
+    const APDS9960_BDATAH = 0x9B
+    const APDS9960_GCONF4 = 0xAB
+    const APDS9960_AICLEAR = 0xE7
+    let color_first_init = false
+
+
+    function i2cwrite_color(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+    function i2cread_color(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+    function rgb2hsl(color_r: number, color_g: number, color_b: number): number {
+        let Hue = 0
+        let R = color_r * 100 / 255;
+        let G = color_g * 100 / 255;
+        let B = color_b * 100 / 255;
+        let maxVal = Math.max(R, Math.max(G, B))
+        let minVal = Math.min(R, Math.min(G, B))
+        let Delta = maxVal - minVal;
+
+        if (Delta < 0) {
+            Hue = 0;
+        }
+        else if (maxVal == R && G >= B) {
+            Hue = (60 * ((G - B) * 100 / Delta)) / 100;
+        }
+        else if (maxVal == R && G < B) {
+            Hue = (60 * ((G - B) * 100 / Delta) + 360 * 100) / 100;
+        }
+        else if (maxVal == G) {
+            Hue = (60 * ((B - R) * 100 / Delta) + 120 * 100) / 100;
+        }
+        else if (maxVal == B) {
+            Hue = (60 * ((R - G) * 100 / Delta) + 240 * 100) / 100;
+        }
+        return Hue
+    }
+    function initModule(): void {
+        i2cwrite_color(APDS9960_ADDR, APDS9960_ATIME, 252)
+        i2cwrite_color(APDS9960_ADDR, APDS9960_CONTROL, 0x03)
+        i2cwrite_color(APDS9960_ADDR, APDS9960_ENABLE, 0x00)
+        i2cwrite_color(APDS9960_ADDR, APDS9960_GCONF4, 0x00)
+        i2cwrite_color(APDS9960_ADDR, APDS9960_AICLEAR, 0x00)
+        i2cwrite_color(APDS9960_ADDR, APDS9960_ENABLE, 0x01)
+        color_first_init = true
+    }
+    function colorMode(): void {
+        let tmp = i2cread_color(APDS9960_ADDR, APDS9960_ENABLE) | 0x2;
+        i2cwrite_color(APDS9960_ADDR, APDS9960_ENABLE, tmp);
+    }
+
+    //% blockId=apds9960_readcolor block="Color sensor IIC port color HUE(0~360)"
+    //% subcategory=Sensor group="IIC Port"
+    export function readColor(): number {
+        if (color_first_init == false) {
+            initModule()
+            colorMode()
+        }
+        let tmp = i2cread_color(APDS9960_ADDR, APDS9960_STATUS) & 0x1;
+        while (!tmp) {
+            basic.pause(5);
+            tmp = i2cread_color(APDS9960_ADDR, APDS9960_STATUS) & 0x1;
+        }
+        let c = i2cread_color(APDS9960_ADDR, APDS9960_CDATAL) + i2cread_color(APDS9960_ADDR, APDS9960_CDATAH) * 256;
+        let r = i2cread_color(APDS9960_ADDR, APDS9960_RDATAL) + i2cread_color(APDS9960_ADDR, APDS9960_RDATAH) * 256;
+        let g = i2cread_color(APDS9960_ADDR, APDS9960_GDATAL) + i2cread_color(APDS9960_ADDR, APDS9960_GDATAH) * 256;
+        let b = i2cread_color(APDS9960_ADDR, APDS9960_BDATAL) + i2cread_color(APDS9960_ADDR, APDS9960_BDATAH) * 256;
+        // map to rgb based on clear channel
+        let avg = c / 3;
+        r = r * 255 / avg;
+        g = g * 255 / avg;
+        b = b * 255 / avg;
+        //let hue = rgb2hue(r, g, b);
+        let hue = rgb2hsl(r, g, b)
+        return hue
+    }
+
+    //% block="Color sensor IIC port detects %color"
+    //% color.fieldEditor="gridpicker" color.fieldOptions.columns=3
+    export function checkColor(color: ColorList): boolean {
+        let hue = readColor()
+        switch (color) {
+            case ColorList.red:
+                if (hue > 330 || hue < 20) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+            case ColorList.green:
+                if (hue > 120 && 180 > hue) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+            case ColorList.blue:
+                if (hue > 210 && 270 > hue) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+            case ColorList.cyan:
+                if (hue > 190 && 210 > hue) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+            case ColorList.magenta:
+                if (hue > 260 && 330 > hue) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+            case ColorList.yellow:
+                if (hue > 30 && 120 > hue) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+            case ColorList.white:
+                if (hue >= 180 && 190 > hue) {
+                    return true
+                }
+                else {
+                    return false
+                }
+                break
+        }
+    }
+}
